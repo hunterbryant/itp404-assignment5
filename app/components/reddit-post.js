9978e818f83ebc0aae7f753a3fe03023e15a34f2
Hunter Bryant <surfaspen@gmail.com>
2017-11-07 16:31:41 -0800

    Assignment 8 Submission

--- app/components/reddit-post.js
+++ app/components/reddit-post.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	originalScore: this.get('post.data.score'),
 	actions: {
 		changeScore(newVal) {
 			this.set('post.data.score', newVal);
